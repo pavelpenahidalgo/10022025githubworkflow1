0123d0e396ead1ba2ab339d08b8df5f17d072a76
Luis Fernando <lcruzf@intercorp.com.pe>
2025-01-29 22:52:31 -0500

    update: vercel version

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-        <div>PROYECTO FRONT PARA PIPELINE -- APUNTANDO A VERCEL 1.0</div>
+        <div>PROYECTO FRONT PARA PIPELINE -- APUNTANDO A VERCEL 1.1</div>
       </header>
     </div>
   );
