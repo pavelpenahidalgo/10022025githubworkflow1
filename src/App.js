ecd8092bcc204d1839d5eb91063cce66168279ee
Luis Fernando <lcruzf@intercorp.com.pe>
2025-01-29 22:58:00 -0500

    update: master version

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-        <div>PROYECTO FRONT PARA PIPELINE VERSION 1.0</div>
+        <div>PROYECTO FRONT PARA PIPELINE VERSION 1.1-Master</div>
       </header>
     </div>
   );
